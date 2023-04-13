3860e3d328a33e24e264f9513dfba137430b1900
Drystan Tchamba <kiberlintchamba2003@gmail.com>
2023-04-14 00:36:59 +0100

    could not update user's peosition

--- app/src/main/java/cm/proj/nimbus/MapsActivity.kt
+++ app/src/main/java/cm/proj/nimbus/MapsActivity.kt
@@ -1,11 +1,16 @@
 package cm.proj.nimbus
 
+import android.Manifest
 import android.Manifest.permission.ACCESS_FINE_LOCATION
 import android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+import android.annotation.SuppressLint
+import android.content.Context
 import android.content.pm.PackageManager
 import android.location.Location
+import android.location.LocationManager
 import android.os.Bundle
 import android.util.Log
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
@@ -18,15 +23,22 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.coroutines.*
 import java.io.File
+import java.text.SimpleDateFormat
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.util.*
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var map: GoogleMap
     private val fileName = "location.nimbus"
+    var state = 1;
     private var file: File = File(fileName)
-    val UPDATE_INTERVAL = 5000L // milliseconds
+    val UPDATE_INTERVAL = 10000L // milliseconds
     val MAP_ZOOM_LEVEL = 15f
+
     // Get the Firebase Firestore instance
     val db = FirebaseFirestore.getInstance()
 
@@ -35,17 +47,22 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
         //initialise loal storage file
-        file= File(this@MapsActivity.getFilesDir(),fileName)
+        file = File(this@MapsActivity.getFilesDir(), fileName)
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
         // Check if the ACCESS_FINE_LOCATION permission was granted before requesting a location
-        if (ContextCompat.checkSelfPermission(this, WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+        if (ContextCompat.checkSelfPermission(
+                this,
+                WRITE_EXTERNAL_STORAGE
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
             // If the permission is not granted, request it
             ActivityCompat.requestPermissions(this, arrayOf(WRITE_EXTERNAL_STORAGE), 1)
         }
         setupLocClient()
+
 
     }
 
@@ -55,7 +72,9 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onMapReady(googleMap: GoogleMap) {
         map = googleMap //initialise map
         getCurrentLocation()
+        startUpdatingLocation(this, map)
     }
+
     private fun setupLocClient() {
         fusedLocClient =
             LocationServices.getFusedLocationProviderClient(this)
@@ -63,73 +82,86 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     // prompt the user to grant/deny access
     private fun requestLocPermissions() {
-        ActivityCompat.requestPermissions(this,
+        ActivityCompat.requestPermissions(
+            this,
             arrayOf(ACCESS_FINE_LOCATION), //permission in the manifest
-            REQUEST_LOCATION)
+            REQUEST_LOCATION
+        )
     }
 
     companion object {
-        private const val REQUEST_LOCATION = 1 //request code to identify specific permission request
+        private const val REQUEST_LOCATION =
+            1 //request code to identify specific permission request
         private const val TAG = "MapsActivity" // for debugging
     }
 
     private fun getCurrentLocation() {
-        if (ActivityCompat.checkSelfPermission(this,
-               ACCESS_FINE_LOCATION) !=
-            PackageManager.PERMISSION_GRANTED) {
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                ACCESS_FINE_LOCATION
+            ) !=
+            PackageManager.PERMISSION_GRANTED
+        ) {
 
             // call requestLocPermissions() if permission isn't granted
             requestLocPermissions()
         } else {
-
+            Log.d(TAG, "Suspicious")
             fusedLocClient.lastLocation.addOnCompleteListener {
                 // lastLocation is a task running in the background
                 val location = it.result //obtain location
                 //reference to the database
-               // val database: FirebaseDatabase = FirebaseDatabase.getInstance()
-               // val ref: DatabaseReference = database.getReference("users")
+                // val database: FirebaseDatabase = FirebaseDatabase.getInstance()
+                // val ref: DatabaseReference = database.getReference("users")
                 if (location != null) {
+                    Log.d(TAG, "Location is not null")
+                    // Getting latest recent position time and date
+                    var time = Calendar.getInstance().time
+                    var formatter = SimpleDateFormat("yyyy-MM-dd HH:mm")
+                    var updst = findViewById<TextView>(R.id.txt_updateStatus)
+                    var current = formatter.format(time)
 
- //Add the data to the "users" collection with an auto-generated document ID
-
-
+                    //Add the data to the "users" collection with an auto-generated document ID
                     val latLng = LatLng(location.latitude, location.longitude)
                     // create a marker at the exact location
-                    map.addMarker(MarkerOptions().position(latLng)
-                        .title("You are currently here!"))
+                    if(state ==1){
+                    map.addMarker(
+                        MarkerOptions().position(latLng)
+                            .title("Initial Position!") //You are currently here
+                    )}else{
+                        map.addMarker(
+                            MarkerOptions().position(latLng)
+                                .title("You are now here!")
+
+                        )
+                        updst.text = "Last Known position was at : $current"
+                        Log.d(TAG, current)
+                    }
+
                     // create an object that will specify how the camera will be updated
                     val update = CameraUpdateFactory.newLatLngZoom(latLng, 16.0f)
                     //Log.d("Firebase", location.toString())
                     map.moveCamera(update)
                     //Save the location data to the database
-                   // ref.setValue(location)
-                    if(!checkFile()){
+                    // ref.setValue(location)
+                    if (!checkFile()) {
                         addData(location, "buses")
-                    }
-                    else{
+                    } else {
                         var ids = writeFile("")
-                        updateDocument(ids,location,"buses")
+                        updateDocument(ids, location, "buses")
                     }
-                    /*db.collection("users")
-                        .add(location)
-                        .addOnSuccessListener { documentReference ->
-                            writeFile(documentReference.id)
-                            Log.d("Firebase", "DocumentSnapshot added with ID: ${documentReference.id}")
-                        }
-                        .addOnFailureListener { e ->
-                            Log.w("Firebase", "Error adding document", e)
-                        }*/
                 } else {
                     // if location is null , log an error message
-                    Log.e(TAG, "No location found")
-                }
+                    Log.d(TAG, "Location is null")
 
+                }
 
 
             }
         }
     }
-    fun addData(location : Location, collectionName: String) {
+
+    fun addData(location: Location, collectionName: String) {
         val collectionRef = db.collection(collectionName)
         collectionRef.add(location)
             .addOnSuccessListener {
@@ -140,6 +172,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
                 println("Error adding data: $e")
             }
     }
+
     fun updateDocument(id: String, newLocation: Location, collectionName: String) {
         val documentRef = db.collection(collectionName).document(id)
 
@@ -160,10 +193,12 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
             e.printStackTrace()
         }
     }
-    fun checkFile() : Boolean{
+
+    fun checkFile(): Boolean {
         return (file.exists())
     }
-    fun writeFile(text : String) : String{
+
+    fun writeFile(text: String): String {
         // check if file exists
 
         if (checkFile()) {
@@ -179,17 +214,19 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
             return text
         }
     }
+
     override fun onRequestPermissionsResult(
         requestCode: Int,
         permissions: Array<String>,
-        grantResults: IntArray) {
+        grantResults: IntArray
+    ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         //check if the request code matches the REQUEST_LOCATION
-        if (requestCode == REQUEST_LOCATION)
-        {
+        if (requestCode == REQUEST_LOCATION) {
             //check if grantResults contains PERMISSION_GRANTED.If it does, call getCurrentLocation()
             if (grantResults.size == 1 && grantResults[0] ==
-                PackageManager.PERMISSION_GRANTED) {
+                PackageManager.PERMISSION_GRANTED
+            ) {
                 getCurrentLocation()
             } else {
                 //if it doesn`t log an error message
@@ -205,5 +242,25 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         }
     }
 
+    @SuppressLint("MissingPermission")
+    fun startUpdatingLocation(context: Context, map: GoogleMap) {
+        val locationManager = context.getSystemService(Context.LOCATION_SERVICE) as LocationManager
 
+        val job = CoroutineScope(Dispatchers.IO).launch {
+            Log.d(TAG, "Launched Coroutine")
+            while (isActive) {
+                setupLocClient()
+                state =2
+                Log.d(TAG, "Set up")
+                delay(UPDATE_INTERVAL)
+            }
+        }
+    }
+
+    fun updateMapMarker(map: GoogleMap, latLng: LatLng) {
+        Log.d(TAG, latLng.toString())
+        map.clear()
+        map.addMarker(MarkerOptions().position(latLng))
+        map.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, MAP_ZOOM_LEVEL))
+    }
 }
