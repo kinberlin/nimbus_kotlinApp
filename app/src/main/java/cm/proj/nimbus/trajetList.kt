572958cf765403c743e31aa22e2549ba7190d026
Drystan Tchamba <kiberlintchamba2003@gmail.com>
2023-04-15 14:59:02 +0100

    correction begining

---
+++ app/src/main/java/cm/proj/nimbus/trajetList.kt
@@ -0,0 +1,11 @@
+package cm.proj.nimbus
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class trajetList : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_trajet_list)
+    }
+}
